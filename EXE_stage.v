9e6532e2ad6493e50ac96a2e2a14fd653ac87461
Lexi <yyqcode@gmail.com>
2019-10-13 14:12:06 +0800

    lab6

--- EXE_stage.v
+++ EXE_stage.v
@@ -25,25 +25,43 @@ wire        es_ready_go   ;
 reg  [`DS_TO_ES_BUS_WD -1:0] ds_to_es_bus_r;
 wire [11:0] es_alu_op     ;
 wire        es_load_op    ;
+wire        es_mul_op     ;
+wire        es_mulu_op    ;
+wire        es_div_op     ;
+wire        es_divu_op    ;
 wire        es_src1_is_sa ;  
 wire        es_src1_is_pc ;
-wire        es_src2_is_imm; 
+wire        es_src2_is_signed_imm;
+wire        es_src2_is_zero_imm;
 wire        es_src2_is_8  ;
 wire        es_gr_we      ;
 wire        es_mem_we     ;
+wire        es_hi_re      ;
+wire        es_lo_re      ;
+wire        es_hi_we      ;
+wire        es_lo_we      ;
 wire [ 4:0] es_dest       ;
 wire [15:0] es_imm        ;
 wire [31:0] es_rs_value   ;
 wire [31:0] es_rt_value   ;
 wire [31:0] es_pc         ;
-assign {es_alu_op      ,  //135:124
-        es_load_op     ,  //123:123
-        es_src1_is_sa  ,  //122:122
-        es_src1_is_pc  ,  //121:121
-        es_src2_is_imm ,  //120:120
-        es_src2_is_8   ,  //119:119
-        es_gr_we       ,  //118:118
-        es_mem_we      ,  //117:117
+assign {es_alu_op      ,  //144:133
+        es_load_op     ,  //132:132
+        es_mul_op      ,  //131:131
+        es_mulu_op     ,  //130:130
+        es_div_op      ,  //129:129
+        es_divu_op     ,  //128:128
+        es_src1_is_sa  ,  //127:127
+        es_src1_is_pc  ,  //126:126
+        es_src2_is_signed_imm ,  //125:125
+        es_src2_is_zero_imm,  //124:124
+        es_src2_is_8   ,  //123:123
+        es_gr_we       ,  //122:122
+        es_mem_we      ,  //121:121
+        es_hi_re       ,  //120:120
+        es_lo_re       ,  //119:119
+        es_hi_we       ,  //118:118
+        es_lo_we       ,  //117:117
         es_dest        ,  //116:112
         es_imm         ,  //111:96
         es_rs_value    ,  //95 :64
@@ -53,7 +71,29 @@ assign {es_alu_op      ,  //135:124
 
 wire [31:0] es_alu_src1   ;
 wire [31:0] es_alu_src2   ;
+wire [31:0] aluout        ;
+
+wire [32:0] prod_src1     ;
+wire [32:0] prod_src2     ;
+wire [63:0] pout          ;
+
+wire dividend_tready; //in
+wire dividend_tvalid; //out
+wire divisor_tready; //in
+wire divisor_tvalid; //out
+wire dout_tvalid; //in
+wire [31:0] dividend      ;
+wire [31:0] divisor       ;
+wire [63:0] dout          ;
+wire [31:0] quotient      ;
+wire [31:0] remainder     ;
+
 wire [31:0] es_alu_result ;
+
+reg  [31:0] hi;
+reg  [31:0] lo;
+wire [31:0] hi_rdata;
+wire [31:0] lo_rdata;
 
 wire        es_res_from_mem;
 
@@ -65,7 +105,9 @@ assign es_to_ms_bus = {es_res_from_mem,  //70:70
                        es_pc             //31:0
                       };
 
-assign es_ready_go    = 1'b1;
+wire es_div_block = (es_div_op || es_divu_op) && !dout_tvalid;
+
+assign es_ready_go    = !es_div_block;
 assign es_allowin     = !es_valid || es_ready_go && ms_allowin;
 assign es_to_ms_valid =  es_valid && es_ready_go;
 always @(posedge clk) begin
@@ -84,16 +126,89 @@ end
 assign es_alu_src1 = es_src1_is_sa  ? {27'b0, es_imm[10:6]} : 
                      es_src1_is_pc  ? es_pc[31:0] :
                                       es_rs_value;
-assign es_alu_src2 = es_src2_is_imm ? {{16{es_imm[15]}}, es_imm[15:0]} : 
-                     es_src2_is_8   ? 32'd8 :
-                                      es_rt_value;
+assign es_alu_src2 = es_src2_is_signed_imm ? {{16{es_imm[15]}}, es_imm[15:0]} : 
+                     es_src2_is_zero_imm   ? {{16{1'b0}}, es_imm[15:0]} : 
+                     es_src2_is_8          ? 32'd8 :
+                                             es_rt_value;
 
 alu u_alu(
     .alu_op     (es_alu_op    ),
     .alu_src1   (es_alu_src1  ), //bug 4
     .alu_src2   (es_alu_src2  ),
-    .alu_result (es_alu_result)
+    .alu_result (aluout       )
     );
+
+assign prod_src1 = es_mul_op ? {es_alu_src1[31], es_alu_src1[31:0]} : 
+                               {1'b0, es_alu_src1[31:0]};
+assign prod_src2 = es_mul_op ? {es_alu_src2[31], es_alu_src2[31:0]} : 
+                               {1'b0, es_alu_src2[31:0]};
+assign pout = $signed(prod_src1) * $signed(prod_src2);
+
+reg dividend_valid;
+assign dividend_tvalid = (es_div_op || es_divu_op) && !dividend_valid;
+always @(posedge clk) begin
+    if (reset)
+        dividend_valid <= 1'b0;
+    else if ((es_div_op || es_divu_op) && dividend_tready)
+        dividend_valid <= 1'b1;
+    else if ((es_div_op || es_divu_op) && dout_tvalid)
+        dividend_valid <= 1'b0;
+end
+
+reg divisor_valid;
+assign divisor_tvalid = (es_div_op || es_divu_op) && !divisor_valid;
+always @(posedge clk) begin
+    if (reset)
+        divisor_valid <= 1'b0;
+    else if ((es_div_op || es_divu_op) && divisor_tready)
+        divisor_valid <= 1'b1;
+    else if ((es_div_op || es_divu_op) && dout_tvalid)
+        divisor_valid <= 1'b0;
+end
+
+assign dividend = es_div_op ? (es_alu_src1[31:0] ^ {32{es_alu_src1[31]}}) + es_alu_src1[31] : 
+                              es_alu_src1[31:0];
+assign divisor = es_div_op ? (es_alu_src2[31:0] ^ {32{es_alu_src2[31]}}) + es_alu_src2[31] : 
+                              es_alu_src2[31:0];
+unsigned_div unsigned_div(
+    .aclk                   (clk),
+    .s_axis_dividend_tdata  (dividend),
+    .s_axis_dividend_tready (dividend_tready),
+    .s_axis_dividend_tvalid (dividend_tvalid),
+    .s_axis_divisor_tdata   (divisor),
+    .s_axis_divisor_tready  (divisor_tready),
+    .s_axis_divisor_tvalid  (divisor_tready),
+    .m_axis_dout_tdata      (dout),
+    .m_axis_dout_tvalid     (dout_tvalid)
+);
+assign quotient  = es_div_op ? (dout[63:32] ^ {32{(es_alu_src1[31] ^ es_alu_src2[31])}}) + (es_alu_src1[31] ^ es_alu_src2[31]) : 
+                               dout[63:32];
+assign remainder = es_div_op ? (dout[31:0] ^ {32{es_alu_src1[31]}}) + es_alu_src1[31] : 
+                               dout[31:0];
+
+always @(posedge clk) begin
+    if (es_mul_op || es_mulu_op)
+        hi <= pout[63:32];
+    else if ((es_div_op || es_divu_op) && dout_tvalid)
+        hi <= remainder;
+    else if (es_hi_we)
+        hi <= es_alu_src1;
+end
+always @(posedge clk) begin
+    if (es_mul_op || es_mulu_op)
+        lo <= pout[31:0];
+    else if ((es_div_op || es_divu_op) && dout_tvalid)
+        lo <= quotient;
+    else if (es_lo_we)
+        lo <= es_alu_src1;
+end
+
+assign hi_rdata = hi;
+assign lo_rdata = lo;
+
+assign es_alu_result = es_hi_re ? hi_rdata : 
+                       es_lo_re ? lo_rdata : 
+                                  aluout;
 
 assign data_sram_en    = 1'b1;
 assign data_sram_wen   = es_mem_we&&es_valid ? 4'hf : 4'h0;
--- ID_stage.v
+++ ID_stage.v
@@ -50,13 +50,22 @@ wire [31:0] br_target;
 
 wire [11:0] alu_op;
 wire        load_op;
+wire        mul_op;
+wire        mulu_op;
+wire        div_op;
+wire        divu_op;
 wire        src1_is_sa;
 wire        src1_is_pc;
-wire        src2_is_imm;
+wire        src2_is_signed_imm;
+wire        src2_is_zero_imm;
 wire        src2_is_8;
 wire        res_from_mem;
 wire        gr_we;
 wire        mem_we;
+wire        hi_re;
+wire        lo_re;
+wire        hi_we;
+wire        lo_we;
 wire [ 4:0] dest;
 wire [15:0] imm;
 wire [31:0] rs_value;
@@ -76,17 +85,36 @@ wire [31:0] rd_d;
 wire [31:0] sa_d;
 wire [63:0] func_d;
 
+wire        inst_add;
 wire        inst_addu;
+wire        inst_sub;
 wire        inst_subu;
 wire        inst_slt;
+wire        inst_slti;
 wire        inst_sltu;
+wire        inst_sltiu;
+wire        inst_mult;
+wire        inst_multu;
+wire        inst_div;
+wire        inst_divu;
+wire        inst_mfhi;
+wire        inst_mflo;
+wire        inst_mthi;
+wire        inst_mtlo;
 wire        inst_and;
+wire        inst_andi;
 wire        inst_or;
+wire        inst_ori;
 wire        inst_xor;
+wire        inst_xori;
 wire        inst_nor;
 wire        inst_sll;
+wire        inst_sllv;
 wire        inst_srl;
+wire        inst_srlv;
 wire        inst_sra;
+wire        inst_srav;
+wire        inst_addi;
 wire        inst_addiu;
 wire        inst_lui;
 wire        inst_lw;
@@ -108,14 +136,23 @@ wire        rs_eq_rt;
 
 assign br_bus       = {br_taken,br_target};
 
-assign ds_to_es_bus = {alu_op      ,  //135:124
-                       load_op     ,  //123:123
-                       src1_is_sa  ,  //122:122
-                       src1_is_pc  ,  //121:121
-                       src2_is_imm ,  //120:120
-                       src2_is_8   ,  //119:119
-                       gr_we       ,  //118:118
-                       mem_we      ,  //117:117
+assign ds_to_es_bus = {alu_op      ,  //144:133
+                       load_op     ,  //132:132
+                       mul_op      ,  //131:131
+                       mulu_op     ,  //130:130
+                       div_op      ,  //129:129
+                       divu_op     ,  //128:128
+                       src1_is_sa  ,  //127:127
+                       src1_is_pc  ,  //126:126
+                       src2_is_signed_imm ,  //125:125
+                       src2_is_zero_imm,  //124:124
+                       src2_is_8   ,  //123:123
+                       gr_we       ,  //122:122
+                       mem_we      ,  //121:121
+                       hi_re       ,  //120:120
+                       lo_re       ,  //119:119
+                       hi_we       ,  //118:118
+                       lo_we       ,  //117:117
                        dest        ,  //116:112
                        imm         ,  //111:96
                        rs_value    ,  //95 :64
@@ -124,11 +161,11 @@ assign ds_to_es_bus = {alu_op      ,  //135:124
                       };
 
 //exe_stage
-wire es_res_from_mem;
+wire es_load_op;
 wire es_gr_we;
 wire [4:0] es_dest;
 wire [31:0] es_alu_result;
-assign es_res_from_mem = es_to_ms_bus[70];
+assign es_load_op = es_to_ms_bus[70];
 assign es_gr_we = es_to_ms_bus[69];
 assign es_dest = es_to_ms_bus[68:64];
 assign es_alu_result = es_to_ms_bus[63:32];
@@ -141,10 +178,10 @@ assign ms_gr_we = ms_to_ws_bus[69];
 assign ms_dest = ms_to_ws_bus[68:64];
 assign ms_final_result = ms_to_ws_bus[63:32];
 
-wire es_lw;
-assign es_lw = es_valid && es_res_from_mem && (es_dest == rs || es_dest == rt);
+wire es_load_block;
+assign es_load_block = es_valid && es_load_op && es_dest != 5'b0 && (es_dest == rs || es_dest == rt);
 
-assign ds_ready_go    = !es_lw;
+assign ds_ready_go    = !es_load_block;
 assign ds_allowin     = !ds_valid || ds_ready_go && es_allowin;
 assign ds_to_es_valid = ds_valid && ds_ready_go;
 always @(posedge clk) begin
@@ -177,7 +214,17 @@ decoder_5_32 u_dec3(.in(rt  ), .out(rt_d  ));
 decoder_5_32 u_dec4(.in(rd  ), .out(rd_d  ));
 decoder_5_32 u_dec5(.in(sa  ), .out(sa_d  ));
 
+assign inst_mfhi   = op_d[6'h00] & func_d[6'h10] & rs_d[5'h00] & rt_d[5'h00] & sa_d[5'h00];
+assign inst_mthi   = op_d[6'h00] & func_d[6'h11] & rt_d[5'h00] & rd_d[5'h00] & sa_d[5'h00];
+assign inst_mflo   = op_d[6'h00] & func_d[6'h12] & rs_d[5'h00] & rt_d[5'h00] & sa_d[5'h00];
+assign inst_mtlo   = op_d[6'h00] & func_d[6'h13] & rt_d[5'h00] & rd_d[5'h00] & sa_d[5'h00];
+assign inst_mult   = op_d[6'h00] & func_d[6'h18] & sa_d[5'h00] & rd_d[5'h00];
+assign inst_multu  = op_d[6'h00] & func_d[6'h19] & sa_d[5'h00] & rd_d[5'h00];
+assign inst_div    = op_d[6'h00] & func_d[6'h1a] & sa_d[5'h00] & rd_d[5'h00];
+assign inst_divu   = op_d[6'h00] & func_d[6'h1b] & sa_d[5'h00] & rd_d[5'h00];
+assign inst_add    = op_d[6'h00] & func_d[6'h20] & sa_d[5'h00];
 assign inst_addu   = op_d[6'h00] & func_d[6'h21] & sa_d[5'h00];
+assign inst_sub    = op_d[6'h00] & func_d[6'h22] & sa_d[5'h00];
 assign inst_subu   = op_d[6'h00] & func_d[6'h23] & sa_d[5'h00];
 assign inst_slt    = op_d[6'h00] & func_d[6'h2a] & sa_d[5'h00];
 assign inst_sltu   = op_d[6'h00] & func_d[6'h2b] & sa_d[5'h00];
@@ -185,10 +232,19 @@ assign inst_and    = op_d[6'h00] & func_d[6'h24] & sa_d[5'h00];
 assign inst_or     = op_d[6'h00] & func_d[6'h25] & sa_d[5'h00];
 assign inst_xor    = op_d[6'h00] & func_d[6'h26] & sa_d[5'h00];
 assign inst_nor    = op_d[6'h00] & func_d[6'h27] & sa_d[5'h00];
+assign inst_sllv   = op_d[6'h00] & func_d[6'h04] & sa_d[5'h00];
+assign inst_srlv   = op_d[6'h00] & func_d[6'h06] & sa_d[5'h00];
+assign inst_srav   = op_d[6'h00] & func_d[6'h07] & sa_d[5'h00];
 assign inst_sll    = op_d[6'h00] & func_d[6'h00] & rs_d[5'h00];
 assign inst_srl    = op_d[6'h00] & func_d[6'h02] & rs_d[5'h00];
 assign inst_sra    = op_d[6'h00] & func_d[6'h03] & rs_d[5'h00];
+assign inst_addi   = op_d[6'h08];
 assign inst_addiu  = op_d[6'h09];
+assign inst_slti   = op_d[6'h0a];
+assign inst_sltiu  = op_d[6'h0b];
+assign inst_andi   = op_d[6'h0c];
+assign inst_ori    = op_d[6'h0d];
+assign inst_xori   = op_d[6'h0e];
 assign inst_lui    = op_d[6'h0f] & rs_d[5'h00];
 assign inst_lw     = op_d[6'h23];
 assign inst_sw     = op_d[6'h2b];
@@ -197,28 +253,37 @@ assign inst_bne    = op_d[6'h05];
 assign inst_jal    = op_d[6'h03];
 assign inst_jr     = op_d[6'h00] & func_d[6'h08] & rt_d[5'h00] & rd_d[5'h00] & sa_d[5'h00];
 
-assign alu_op[ 0] = inst_addu | inst_addiu | inst_lw | inst_sw | inst_jal;
-assign alu_op[ 1] = inst_subu;
-assign alu_op[ 2] = inst_slt;
-assign alu_op[ 3] = inst_sltu;
-assign alu_op[ 4] = inst_and;
+assign alu_op[ 0] = inst_add | inst_addu | inst_addi | inst_addiu | inst_lw | inst_sw | inst_jal;
+assign alu_op[ 1] = inst_sub | inst_subu;
+assign alu_op[ 2] = inst_slt | inst_slti;
+assign alu_op[ 3] = inst_sltu | inst_sltiu;
+assign alu_op[ 4] = inst_and | inst_andi;
 assign alu_op[ 5] = inst_nor;
-assign alu_op[ 6] = inst_or;
-assign alu_op[ 7] = inst_xor;
-assign alu_op[ 8] = inst_sll;
-assign alu_op[ 9] = inst_srl;
-assign alu_op[10] = inst_sra;
+assign alu_op[ 6] = inst_or | inst_ori;
+assign alu_op[ 7] = inst_xor | inst_xori;
+assign alu_op[ 8] = inst_sll | inst_sllv;
+assign alu_op[ 9] = inst_srl | inst_srlv;
+assign alu_op[10] = inst_sra | inst_srav;
 assign alu_op[11] = inst_lui;
 assign load_op = inst_lw; //bug 1
+assign mul_op  = inst_mult;
+assign mulu_op = inst_multu;
+assign div_op  = inst_div;
+assign divu_op = inst_divu;
 assign src1_is_sa   = inst_sll   | inst_srl | inst_sra;
 assign src1_is_pc   = inst_jal;
-assign src2_is_imm  = inst_addiu | inst_lui | inst_lw | inst_sw;
+assign src2_is_signed_imm  = inst_addi | inst_addiu | inst_slti | inst_sltiu | inst_lui | inst_lw | inst_sw;
+assign src2_is_zero_imm = inst_andi | inst_ori | inst_xori;
 assign src2_is_8    = inst_jal;
 assign res_from_mem = inst_lw;
 assign dst_is_r31   = inst_jal;
-assign dst_is_rt    = inst_addiu | inst_lui | inst_lw;
-assign gr_we        = ~inst_sw & ~inst_beq & ~inst_bne & ~inst_jr;
+assign dst_is_rt    = inst_addi | inst_addiu | inst_slti | inst_sltiu | inst_andi | inst_ori | inst_xori | inst_lui | inst_lw;
+assign gr_we        = ~inst_sw & ~inst_beq & ~inst_bne & ~inst_jr & ~inst_mult & ~inst_multu & ~inst_div & ~inst_divu & ~inst_mthi & ~inst_mtlo;
 assign mem_we       = inst_sw;
+assign hi_re        = inst_mfhi;
+assign lo_re        = inst_mflo;
+assign hi_we        = inst_mthi;
+assign lo_we        = inst_mtlo;
 
 assign dest         = dst_is_r31 ? 5'd31 :
                       dst_is_rt  ? rt    : 
